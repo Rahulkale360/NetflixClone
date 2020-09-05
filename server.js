9b447700791b6924a445803298cd8c335d7b11a2
unknown <rahulkale360@gmail.com>
2020-09-05 19:56:12 +0530

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 app.use(express.static(__dirname + 'dist/netflixclone'));
 
-PathLocationStrategy
 app.get('',function(req, res){
     res.sendFile(path.join(__dirname + 'dist/netflixclone/index.html'))
 })
